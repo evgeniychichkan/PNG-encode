78e4c5523d28013f02a4b6b12eaf13abe2bb4272
Evgeniy <chichkan.evgeniy@gmail.com>
2016-05-20 19:19:27 +0300

    started correctly perform an encode and decode messages

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
-//Подключение необходимых NPM модулей
-//----------------------------------------------------------------------------------
 
+//Подключение необходимых NPM модулей
 var fs = require('fs'),
     PNG = require('pngjs').PNG;
-
+// Переменные для работы с файлом и флаг режима работы
 var srcFile = process.argv[2],
     flag = process.argv[3],
     dstFile = process.argv[6] || 'out.png';
-
+// Объект для работы с PNG изображением
 var png = new PNG({
     filterType: 4
 });
-// var decMsg =[],
-//     decPtrnW = 0,
-//     decPtrnH = 0;
-var imgPxArray = [];
-var aSCIIArrayMsgDecode = [];
-var aSCIIArrayMsg = [];
 
 //-----------------------------------------------------------------------------------
-// Преобразование строки сообщения в коды таблицы ASCII и запись их в массив
+//   Преобразование строки сообщения в коды таблицы ASCII и запись их в массив
 //----------------------------------------------------------------------------------
-
 function arrayConvertToNumbers(msg, msgLength) {
+  var msgArray = [];
     for (var i = 0; i < msgLength; i++) {
-        aSCIIArrayMsg.push(msg.charCodeAt(i));
+        msgArray.push(msg.charCodeAt(i));
     }
-    console.log(aSCIIArrayMsg);
-    return aSCIIArrayMsg;
+    return msgArray;
 }
 
-
 //-----------------------------------------------------------------------------------
-// Преобразование массива из кодов таблицы ASCII в строку сообщения
-//----------------------------------------------------------------------------------
-function arrayConvertToString(aSCIIArrayMsgDecode) {
+//   Преобразование массива из кодов таблицы ASCII в строку сообщения
+//-----------------------------------------------------------------------------------
+function arrayConvertToString(MsgDecode) {
     var msg = '';
-    for (var i = 0; i < msgLength; i++) {
-        msg += String.fromCharCode(aSCIIArrayMsgDecode.pop());
+    for (var i = 0; i < MsgDecode.length; i++) {
+        msg += String.fromCharCode(MsgDecode[i]);
     }
-    console.log(aSCIIArrayMsgDecode);
     return msg;
 }
 
@@ -50,100 +40,108 @@ function arrayConvertToString(aSCIIArrayMsgDecode) {
  -decode - декодирвоание)
 */
 //----------------------------------------------------------------------------------
-
 function whatToDo(flag) {
+    // Логика программы, если получен флаг на кодирование '-encode'
     if (flag == '-encode') {
         console.log('Start encoding file...\n');
 
         var msg = process.argv[4],
             msgLength = process.argv[4].length,
             pattern = process.argv[5].split('x'),
-            patternWidth = pattern[0],
-            patternHeight = pattern[1];
+            patternWidth = parseInt(pattern[0]),
+            patternHeight = parseInt(pattern[1]);
+        //Конвертирование полученного сообщения в архив из кодов символов
+        var arrayMsgASCII = arrayConvertToNumbers(msg, msgLength);
 
-        arrayConvertToNumbers(msg, msgLength);
-        encodingFile(aSCIIArrayMsg, msgLength, patternWidth, patternHeight);
-
+        /* Функция кодирования сообщения.
+          Получает параметрами:
+          - архив ASCII кодов символов сообщения;
+          - длину сообщения;
+          - шаг кодирования по ширине;
+          - шаг кодирования по высоте.
+          */
+        encodingFile(arrayMsgASCII, msgLength, patternWidth, patternHeight);
+        // Вывод результата работы
         console.log(srcFile + ' has encoded, message = \"' + msg + '\", pattern = ' + pattern.join("x"));
 
+    // Логика программы, если получен флаг на декодирование '-decode'
     } else if (flag == '-decode') {
         console.log('Start decoding file...\n');
-
-         var decodeArray =  decodingFile(srcFile);
-        //  var msgDecode = arrayConvertToString(decodeArray[0]);
-
-        console.log(1, decodeArray);
-        // console.log(srcFile + ' was decoded, message = \"' +
-        // //  msgDecode+
-        //  '\", pattern = ' +  decodeArray[1] + 'x' + decodeArray[2]);
+        /* Функция декодирования сообщения.
+          Получает параметрами:
+          - имя файла для декодирования
+          */
+        var decodeArray = decodingFile(srcFile);
+        // console.log(1, decodeArray);
     }
 }
 
 //----------------------------------------------------------------------------------
 //  Загрузка файла и кодирование сообщения msg в файл dstFile, используя шаг pattern
 //----------------------------------------------------------------------------------
-
-function encodingFile(aSCIIArrayMsg, msgLength, patternWidth, patternHeight) {
+function encodingFile(msgArrayASCII, msgLength, patternWidth, patternHeight) {
     fs.createReadStream(srcFile)
         .pipe(png)
         .on('parsed', function() {
             var i = 0;
             for (var y = 0; y < this.height; y += patternHeight) {
                 for (var x = 0; x < this.width; x += patternWidth) {
-                  if (i >= aSCIIArrayMsg.length) {
-    // // Alpha
-                    this.data[x+y] = aSCIIArrayMsg[i];
-                    i++;
-                  } else break;
-                }
-            }
-            this.data[this.width + this.height] = patternHeight;
-            this.data[this.height] = patternWidth;
-            this.data[0] = msgLength;
-            console.log('\nЗначение png.data: ' + this.data.length);
-            this.pack().pipe(dst = fs.createWriteStream(dstFile));
-
-        });
-}
-
-//----------------------------------------------------------------------------------
-// Декодирование сообщения в msg из файла srcFile
-//----------------------------------------------------------------------------------
-
-function decodingFile(srcFile) {
-    var patternWidth,
-        patternHeight;
-    fs.createReadStream(srcFile)
-        .pipe(png)
-        .on('parsed', function() {
-            var i = 0;
-            patternWidth = this.data[this.height];
-            console.log(patternWidth + '\n');
-            patternHeight = this.data[this.width + this.height];
-            console.log(patternHeight + '\n');
-            var msgLength = this.data[0];
-            console.log(msgLength + '\n');
-
-            for (var y = 0; y < this.height; y += patternHeight) {
-                for (var x = 0; x < this.width; x += patternWidth) {
-                    // Alpha
-                    if (i < msgLength) {
-                        aSCIIArrayMsgDecode.push(this.data[x + y]);
+                    // Запись очередной буквы в виде ее ASCII кода в значение альфа-канала пикселя
+                    if (i !== msgLength) {
+                        var indx = (this.width * y + x) * 4;
+                        this.data[indx + 3] = msgArrayASCII[i];
                         i++;
                     }
                 }
             }
-
-            console.log('\nЗначение png.data: ' + this.data.length);
-            console.log(srcFile + ' was decoded, message = \"' + aSCIIArrayMsgDecode + '\", pattern = ' + patternWidth + 'x' + patternHeight);
+            //Запись шага кодирования по высоте
+            this.data[this.width * this.height] = patternHeight;
+            //Запись шага кодирования по ширине
+            this.data[this.height] = patternWidth;
+            //Запись длины сообщения
+            this.data[0] = msgLength;
+            //Запись массива данных пикселей data в новый файл
+            this.pack().pipe(dst = fs.createWriteStream(dstFile));
         });
-      return [aSCIIArrayMsgDecode, patternWidth, patternHeight];
+}
+
+
+//----------------------------------------------------------------------------------
+// Декодирование сообщения в msg из файла srcFile
+//----------------------------------------------------------------------------------
+function decodingFile(srcFile) {
+    fs.createReadStream(srcFile)
+        .pipe(png)
+        .on('parsed', function() {
+            var i = 0;
+            var msgDecodeArrayASCII = [];
+            //Получение шага декодирования по ширине
+            var patternWidth = this.data[this.height];
+            //Получение шага декодирования по высоте
+            var patternHeight = this.data[this.width * this.height];
+            //Получение длины декодируемого сообщения
+            var msgLength = this.data[0];
+
+            for (var y = 0; y < this.height; y += patternHeight) {
+                for (var x = 0; x < this.width; x += patternWidth) {
+                    // Чтение ASCII кода очередного символа декодируемого сообщения
+                    if (i !== msgLength) {
+                        var indx = (this.width * y + x) * 4;
+                        // console.log(indx);
+                        msgDecodeArrayASCII.push(this.data[indx + 3]);
+                        i++;
+                    }
+                }
+            }
+            //Вывод результата декодирования
+            console.log(srcFile + ' was decoded, message = \"' + arrayConvertToString(msgDecodeArrayASCII) + '\", pattern = ' + patternWidth + 'x' + patternHeight);
+            return [msgDecodeArrayASCII, patternWidth, patternHeight];
+        });
+
 }
 
 //------------------------------------------------------------------------------------
 //Тело программы
 //----------------------------------------------------------------------------------
-
 console.log("Encode/Decode message to PNG image.\n");
-
 whatToDo(flag);
