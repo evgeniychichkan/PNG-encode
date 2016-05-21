074502cc0e2626c39df44b273f7e2540dc502ecc
Evgeniy <chichkan.evgeniy@gmail.com>
2016-05-21 23:50:20 +0300

    added a downloading image by url

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-
 //Подключение необходимых NPM модулей
 var fs = require('fs'),
-    PNG = require('pngjs').PNG;
-// Переменные для работы с файлом и флаг режима работы
+    PNG = require('pngjs').PNG,
+    http = require('http');
+request = require('request');
+
+// Переменные для работы с файлами
 var srcFile = process.argv[2],
     flag = process.argv[3],
-    dstFile = process.argv[6] || 'out.png';
+    dstFile = process.argv[6];
+
+var srcFileLocal = 'out.png',
+    regexp = /(http|ftp|https|www):/gi,
+    testURL = srcFile.match(regexp);
+
 // Объект для работы с PNG изображением
 var png = new PNG({
     filterType: 4
@@ -15,12 +22,13 @@ var png = new PNG({
 //   Преобразование строки сообщения в коды таблицы ASCII и запись их в массив
 //----------------------------------------------------------------------------------
 function arrayConvertToNumbers(msg, msgLength) {
-  var msgArray = [];
+    var msgArray = [];
     for (var i = 0; i < msgLength; i++) {
         msgArray.push(msg.charCodeAt(i));
     }
     return msgArray;
 }
+
 
 //-----------------------------------------------------------------------------------
 //   Преобразование массива из кодов таблицы ASCII в строку сообщения
@@ -34,17 +42,42 @@ function arrayConvertToString(MsgDecode) {
 }
 
 //-----------------------------------------------------------------------------------
-/*
-Функция выбора режима работы с изображением по значению флага
+// Загрузка изображения для кодирования по url
+//-----------------------------------------------------------------------------------
+function fromWhereFile(srcFile, filename) {
+    var request = http.get(srcFile, function(response) {
+        if (response.statusCode === 200) {
+            response.pipe(fs.createWriteStream(filename))
+                .on('close', function() {
+                    // console.log('Downloading done.');
+                })
+                .on('error', function() {
+                    console.log('File don\'t exists!');
+                });
+            // Ожидание скачивания файла
+            request.setTimeout(800, function() {
+                request.abort();
+                console.log('Downloading is abort\nTry again or check URL...\n');
+            });
+        }
+    });
+}
+
+//-----------------------------------------------------------------------------------
+/* Функция выбора режима работы с изображением по значению флага
 (-encode - кодирование или
  -decode - декодирвоание)
 */
 //----------------------------------------------------------------------------------
 function whatToDo(flag) {
+    // Загрузка файла по http://
+    if (testURL == 'http:' || testURL == 'www') {
+        fromWhereFile(srcFile, srcFileLocal);
+    }
+    //----------------------------------------------------------------------------------
     // Логика программы, если получен флаг на кодирование '-encode'
     if (flag == '-encode') {
         console.log('Start encoding file...\n');
-
         var msg = process.argv[4],
             msgLength = process.argv[4].length,
             pattern = process.argv[5].split('x'),
@@ -52,7 +85,6 @@ function whatToDo(flag) {
             patternHeight = parseInt(pattern[1]);
         //Конвертирование полученного сообщения в архив из кодов символов
         var arrayMsgASCII = arrayConvertToNumbers(msg, msgLength);
-
         /* Функция кодирования сообщения.
           Получает параметрами:
           - архив ASCII кодов символов сообщения;
@@ -60,27 +92,24 @@ function whatToDo(flag) {
           - шаг кодирования по ширине;
           - шаг кодирования по высоте.
           */
-        encodingFile(arrayMsgASCII, msgLength, patternWidth, patternHeight);
-        // Вывод результата работы
-        console.log(srcFile + ' has encoded, message = \"' + msg + '\", pattern = ' + pattern.join("x"));
-
-    // Логика программы, если получен флаг на декодирование '-decode'
+        encodingFile(srcFileLocal, arrayMsgASCII, msgLength, msg, patternWidth, patternHeight);
+        //----------------------------------------------------------------------------------
+        // Логика программы, если получен флаг на декодирование '-decode'
     } else if (flag == '-decode') {
         console.log('Start decoding file...\n');
         /* Функция декодирования сообщения.
           Получает параметрами:
           - имя файла для декодирования
           */
-        var decodeArray = decodingFile(srcFile);
-        // console.log(1, decodeArray);
+        decodingFile(srcFile);
     }
 }
 
 //----------------------------------------------------------------------------------
 //  Загрузка файла и кодирование сообщения msg в файл dstFile, используя шаг pattern
 //----------------------------------------------------------------------------------
-function encodingFile(msgArrayASCII, msgLength, patternWidth, patternHeight) {
-    fs.createReadStream(srcFile)
+function encodingFile(srcFileEncode, msgArrayASCII, msgLength, msg, patternWidth, patternHeight) {
+    fs.createReadStream(srcFileEncode)
         .pipe(png)
         .on('parsed', function() {
             var i = 0;
@@ -101,16 +130,18 @@ function encodingFile(msgArrayASCII, msgLength, patternWidth, patternHeight) {
             //Запись длины сообщения
             this.data[0] = msgLength;
             //Запись массива данных пикселей data в новый файл
-            this.pack().pipe(dst = fs.createWriteStream(dstFile));
+            this.pack()
+                .pipe(fs.createWriteStream(dstFile));
+            // Вывод результата кодирования
+            console.log(srcFile + ' has encoded, message = \"' + msg + '\", pattern = ' + patternWidth + 'x' + patternHeight);
         });
 }
-
 
 //----------------------------------------------------------------------------------
 // Декодирование сообщения в msg из файла srcFile
 //----------------------------------------------------------------------------------
-function decodingFile(srcFile) {
-    fs.createReadStream(srcFile)
+function decodingFile(srcFileDecoded) {
+    fs.createReadStream(srcFileDecoded)
         .pipe(png)
         .on('parsed', function() {
             var i = 0;
@@ -127,17 +158,14 @@ function decodingFile(srcFile) {
                     // Чтение ASCII кода очередного символа декодируемого сообщения
                     if (i !== msgLength) {
                         var indx = (this.width * y + x) * 4;
-                        // console.log(indx);
                         msgDecodeArrayASCII.push(this.data[indx + 3]);
                         i++;
                     }
                 }
             }
             //Вывод результата декодирования
-            console.log(srcFile + ' was decoded, message = \"' + arrayConvertToString(msgDecodeArrayASCII) + '\", pattern = ' + patternWidth + 'x' + patternHeight);
-            return [msgDecodeArrayASCII, patternWidth, patternHeight];
+            console.log(srcFileDecoded + ' was decoded, message = \"' + arrayConvertToString(msgDecodeArrayASCII) + '\", pattern = ' + patternWidth + 'x' + patternHeight);
         });
-
 }
 
 //------------------------------------------------------------------------------------
